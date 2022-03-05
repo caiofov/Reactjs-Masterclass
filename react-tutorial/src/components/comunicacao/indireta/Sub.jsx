fcff0fca8b01625e1ad3558e743c99e035e425cd
caiofov <cfoviana@gmail.com>
2022-03-05 18:59:08 -0300

    state 24:45

--- react-tutorial/src/components/comunicacao/indireta/Sub.jsx
+++ react-tutorial/src/components/comunicacao/indireta/Sub.jsx
@@ -5,9 +5,12 @@ export default (props) => {
     return (
     <div>
         <button onClick={() => {
-            props.onClicar(Math.random()) //passando informação para o componente
+            props.onClicar(Math.random(), 'Gerado') //passando informação para o componente
             }}>Alterar</button>
-            {/* A arrow function é importante porque, sem ela, a função onClicar seria executada. Nesse caso, ela está sendo passada como um parâmetro e não sendo executada */}
+            {/* 
+            A arrow function é importante porque, sem ela, a função onClicar seria executada. Nesse caso, ela está sendo passada como um parâmetro e não sendo executada 
+            Poderia ter sido feita uma função em vez de passar a arrow function, mas não será necessario
+            */}
         {/* Quando o botão for clicado, irá chamar a função do componente pai */}
     </div>
     )
--- react-tutorial/src/components/comunicacao/indireta/Super.jsx
+++ react-tutorial/src/components/comunicacao/indireta/Super.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useState} from "react"; //com o useState, podemos criar dados que podem ser modificados
 import Sub from "./Sub";
 
 
 //tipo de comunicação indireta -> filho passa informaçao pro pai mesmo sem ter uma referência dele
 export default props =>{
-    function quandoClicar(valorGerado){
-        alert(valorGerado)
+    const [num, setNum] = useState(0) //num -> dado que será alterado || setNum -> função que será responsável pela alteração do dado
+    const [texto, setTexto] = useState("Valor")
+    
+    function quandoClicar(valorGerado, texto){
+        setNum(valorGerado) //sempre que clicar, mudará o valor gerado
+        setTexto(texto)
     }
     
     return(
     <div>
-        <h4>Valor</h4>
-        <Sub onClicar={quandoClicar}>Pedro</Sub>
+        <h4>{texto}: {num} </h4>
+        <Sub onClicar={quandoClicar}></Sub>
     </div>
     )
 }
